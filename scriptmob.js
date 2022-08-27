e39ade8e47a5b736ea6c2fc976e5b5c15b9ec022
Shomy032 <milosmilic032@gmail.com>
2022-08-28 01:10:05 +0200

    formating | 28/08/2022

--- scriptmob.js
+++ scriptmob.js
@@ -1,14 +1,9 @@
 
 
-function myFunction(){
+function myFunction() {
   var elem1 = document.getElementById("card1");
   var elem2 = document.getElementById("card2");
 
-elem1.classList.toggle("desno1mob");
-elem2.classList.toggle("desno2mob");
+  elem1.classList.toggle("desno1mob");
+  elem2.classList.toggle("desno2mob");
 }
-
-
-
-
-// element.classList.toggle("mystyle");
